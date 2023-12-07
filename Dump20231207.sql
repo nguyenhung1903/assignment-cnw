44f9b13ca72d457e4dd3e0cec328e1f96d780cde
Lighttx26 <ncnhatquang26@gmail.com>
2023-12-07 16:30:16 +0700

    database

---
+++ Dump20231207.sql
@@ -0,0 +1,144 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cnw
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `CategoryId` varchar(45) NOT NULL,
+  `CategoryName` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`CategoryId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES ('1','Food'),('2','Drink');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order` (
+  `OrderId` varchar(45) NOT NULL,
+  `CustomerId` varchar(45) NOT NULL,
+  `ProductId` varchar(45) NOT NULL,
+  `Quantity` int NOT NULL,
+  `Time` datetime DEFAULT NULL,
+  `PhoneNumber` varchar(45) DEFAULT NULL,
+  `Address` varchar(45) DEFAULT NULL,
+  `IsDelivered` tinyint DEFAULT NULL,
+  PRIMARY KEY (`OrderId`),
+  KEY `product_idx` (`ProductId`),
+  KEY `customer_idx` (`CustomerId`),
+  CONSTRAINT `customer` FOREIGN KEY (`CustomerId`) REFERENCES `user` (`UserId`),
+  CONSTRAINT `product` FOREIGN KEY (`ProductId`) REFERENCES `product` (`ProductId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+INSERT INTO `order` VALUES ('ord1','ctm1','prd1',3,'2023-12-04 00:00:00',NULL,NULL,0),('ord2','ctm1','prd2',2,'2023-10-04 00:00:00',NULL,NULL,1),('ord3','ctm2','prd4',1,'2023-11-04 00:00:00',NULL,NULL,1),('ord4','ctm3','prd1',2,'2023-12-04 00:00:00',NULL,NULL,0),('ord5','ctm3','prd1',1,'2023-09-04 00:00:00',NULL,NULL,1);
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `ProductId` varchar(45) NOT NULL,
+  `ProductName` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `CategoryId` varchar(45) NOT NULL,
+  `Price` double NOT NULL,
+  `IsAvailable` tinyint DEFAULT NULL,
+  PRIMARY KEY (`ProductId`),
+  KEY `category_idx` (`CategoryId`),
+  CONSTRAINT `category` FOREIGN KEY (`CategoryId`) REFERENCES `category` (`CategoryId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES ('prd1','Coca 450ml','2',25000,1),('prd2','Coca 330ml','2',18000,1),('prd3','Đùi gà rán','1',45000,1),('prd4','Cánh gà rán','1',40000,1),('prd5','Pepsi 330ml','2',18000,0),('prd6','Hamburger lớn','1',35000,1),('prd7','Hamburger vừa','1',30000,0),('prd8','Xúc xích','1',20000,0);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `UserId` varchar(45) NOT NULL,
+  `Username` varchar(45) NOT NULL,
+  `Password` varchar(45) NOT NULL,
+  `Role` varchar(45) NOT NULL,
+  `Fullname` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `PhoneNumber` varchar(45) DEFAULT NULL,
+  `Address` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  PRIMARY KEY (`UserId`),
+  UNIQUE KEY `Username_UNIQUE` (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('adm1','admin1','admin','admin','Admin số 1',NULL,NULL),('adm2','admin2','admin','admin','Admin số 2',NULL,NULL),('ctm1','customer1','customer','customer','Nguyễn Văn A','0123456789','Hòa Khánh Bắc, Liên Chiểu, Đà Nẵng'),('ctm2','customer2','customer','customer','Nguyễn Văn B','0234567891','Hòa Khánh Nam, Liên Chiểu, Đà Nẵng'),('ctm3','customer3','customer','customer','Nguyễn Văn C','0345678912','Hải Châu, Đà Nẵng');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-07 16:28:30
