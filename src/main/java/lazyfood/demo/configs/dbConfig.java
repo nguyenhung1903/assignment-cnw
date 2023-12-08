c378759fca1d8a055dcac402a6f0686dae079701
An Hung Nguyen <nguyenanhung1903@gmail.com>
2023-12-08 16:17:12 +0700

    add dbConfig

---
+++ src/main/java/lazyfood/demo/configs/dbConfig.java
@@ -0,0 +1,8 @@
+package lazyfood.demo.configs;
+
+public class dbConfig {
+    public static String driverClassName = "com.mysql.cj.jdbc.Driver";
+    public static String dbUrl = "jdbc:mysql://localhost:3306/cnw";
+    public static String username = "root";
+    public static String password = "nguyenhung1903";
+}
